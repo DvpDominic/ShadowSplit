380cd79558ba2eb7a690cff19d789466c98cb5e1
Darshan Parmar <parmardarshanv@gmail.com>
2025-07-16 23:28:58 +0530

    writing player script

--- Game/Scripts/Player.gd
+++ Game/Scripts/Player.gd
@@ -1,23 +1,25 @@
 extends CharacterBody2D
 
-const speed: float = 100
-const jump_force: float = -200
-const gravity = 600
 
-var vel = Vector2.ZERO
+const SPEED = 150.0
+const JUMP_VELOCITY = -400.0
 
-func _physics_process(delta):
-	vel.x = 0
-	
-	if Input.is_action_pressed("ui_right"):
-		vel.x += speed
-	elif Input.is_action_pressed("ui_right"):
-		vel.x -= speed
-	
-	if is_on_floor():
-		if Input.is_action_just_pressed("jump"):
-			vel.y = jump_force
+
+func _physics_process(delta: float) -> void:
+	# Add the gravity.
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+
+	# Handle jump.
+	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var direction := Input.get_axis("ui_left", "ui_right")
+	if direction:
+		velocity.x = direction * SPEED
 	else:
-		vel.y += gravity * delta
-		
-	
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+
+	move_and_slide()
